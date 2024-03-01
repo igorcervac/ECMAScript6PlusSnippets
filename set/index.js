6876c9a179622a1e3691514620fcc62450285281
Igor Cervac <igor.cervac@outlook.com>
2024-03-01 07:05:47 -0500

    Set

---
+++ set/index.js
@@ -0,0 +1,24 @@
+let numberSet = new Set([1,3]);
+numberSet.add(5).add(7);
+
+let numberSet2 = new Set([2,4,8]);
+
+console.group("odds");
+numberSet.forEach(v => console.log(v));
+console.groupEnd("odds");
+console.log("odds size:",numberSet.size);
+
+console.group("evens");
+numberSet2.forEach(v => console.log(v));
+console.groupEnd("evens");
+console.log("evens size:",numberSet2.size);
+
+console.group("intersection");
+let intersectedSet = numberSet.intersection(numberSet2);
+intersectedSet.forEach(v => console.log(v));
+console.groupEnd("intersection");
+
+console.group("union");
+let unionSet = numberSet.union(numberSet2);
+unionSet.forEach(v => console.log(v));
+console.groupEnd("union");
